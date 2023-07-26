e979fecf2974f98145aeef845012a86bb4795dcc
Kian Slagter <kianslagter@gmail.com>
2023-07-26 15:13:19 +0930

    prac

---
+++ function-2-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int max_element(int array[], int n){
+    // initailise maxNum
+    int maxNumber = array[0];
+    // for loop to go through array
+    for (int i = 0; i < n; i++) {
+        if (array[i] > maxNumber){
+            maxNumber = array[i];
+        }
+    }
+   
+   return maxNumber;
+}
