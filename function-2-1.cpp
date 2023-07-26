1588d160e8266681935cf136fd10cf2ee6c38e2c
Kian Slagter <kianslagter@gmail.com>
2023-07-26 14:40:50 +0930

    prac

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -2,9 +2,9 @@
 
 int min_element(int array[], int n){
     // initailise minNum
-    int minNumber = 0;
+    int minNumber = array[0];
     // for loop to go through array
-    for (int i = 1; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         if (array[i] < minNumber){
             minNumber = array[i];
         }
