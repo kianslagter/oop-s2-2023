5f850734214df9c1e6103fbb5f1aa7e47f51de9c
Kian Slagter <kianslagter@gmail.com>
2023-07-28 14:06:06 +0930

    prac

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -5,7 +5,7 @@ int count_evens(int number){
     int evenCount = 0;
     if (number < 1) return 0;
     // for loop to go through array
-    for (int i = 1; i < number; i++) {
+    for (int i = 0; i < number; i++) {
         if (i % 2 == 0){
             evenCount++;
         }
