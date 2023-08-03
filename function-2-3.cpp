513db6276b374bfae4c6d9ca84a4f7539fa9091e
Kian Slagter <kianslagter@gmail.com>
2023-08-03 18:46:00 +0930

    prac2

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+
 int sum_array_elements(int integers[], int length) {
+  if (length <= 0)return false;
+  
   int sum = 0;
   for (int i = 0; i < length; i++) {
     sum = sum + integers[i];
@@ -10,6 +13,8 @@ int sum_array_elements(int integers[], int length) {
 }
 
 bool is_palindrome(int integers[], int length) {
+  if (length <= 0)return false;
+
   for (int i = 0; i < floor(length/2); i++) {
     if (integers[i] != integers[length-1-i]) {
       return 0;
@@ -19,6 +24,8 @@ bool is_palindrome(int integers[], int length) {
 }
 
 int sum_if_palindrome(int integers[], int length) {
+  if (length <= 0)return false;
+  
   if (is_palindrome(integers, length) == 1) {
     return sum_array_elements(integers, length);
   } else {
