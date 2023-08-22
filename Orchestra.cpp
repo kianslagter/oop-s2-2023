0f9779ec4c40f94ff919d58fd72ff74eb2ebf7b4
Kian Slagter <kianslagter@gmail.com>
2023-08-22 13:11:04 +0930

    workshop

--- Orchestra.cpp
+++ Orchestra.cpp
@@ -1,20 +1,14 @@
 #include "Orchestra.h"
 
 // default constructor
-Orchestra::Orchestra() : size(0), current_members(0), members(new Musician[size]) {}
+Orchestra::Orchestra() : size(0), current_members(0), members(nullptr) {}
 
 // constructor for an orchestra of given size
 Orchestra::Orchestra(int size) : size(size), current_members(0), members(new Musician[size]) {}
 
 // returns the number of musicians who have joined the orchestra
 int Orchestra::get_current_number_of_members() {
-    int count = 0;
-    for (int i = 0; i < size; i++) {
-        if (!members[i].get_instrument().empty()) {
-            ++count;
-        }
-    }
-    return count;
+    return current_members;
 }
 
 // returns true if any musician in the orchestra plays the specified instrument
@@ -36,7 +30,7 @@ Musician* Orchestra::get_members() {
 bool Orchestra::add_musician(Musician new_musician) {
         if (current_members < size) {
             members[current_members] = new_musician;
-            ++current_members;
+            current_members++;
             return true;
         }
         return false;
