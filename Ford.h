ffea13709aaf332aaddbadd5f27656cda023c685
Kian Slagter <kianslagter@gmail.com>
2023-09-07 16:50:31 +0930

    pracexam

--- Ford.h
+++ Ford.h
@@ -12,15 +12,17 @@ class Ford : public Car {
     Ford() : Car(0), badgeNumber() {};
     Ford(int badgeNumber, int price) : badgeNumber(badgeNumber), Car(price), litresOfFuel(60) {};
     
+  
     void drive(int kms){
         int distance = 0;
-        int e = 0;
+        float e = 0;
         while (litresOfFuel > 0 && distance < kms){
-            set_emissions(e + 0.234);
+            e += 0.234;
+            set_emissions(e);
             litresOfFuel = litresOfFuel - 0.2;
             distance++;
     }
-    }
+}
 
     void refuel(int litres){
         if (litresOfFuel + litres >= 60){
@@ -53,14 +55,12 @@ class Ford : public Car {
     }
 
     float get_litresOfFuel(){
-        float roundFuel = 0; 
-        if(litresOfFuel < 0){
-            roundFuel = 0;
+        float roundFuel = round(litresOfFuel * 10) / 10;
+        if (roundFuel < 0) {
+         roundFuel = 0;
         }
-        else{
-           roundFuel = (litresOfFuel * 10) / 10;
-        }
-        return roundFuel;
-    }
+    return roundFuel;
+}
+
 
 };
