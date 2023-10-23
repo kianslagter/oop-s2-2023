33b91202ff4a55c0de8ce5f9a35aebcfe227f5ab
Kian Slagter <kianslagter@gmail.com>
2023-10-23 17:34:00 +1030

    workshop

--- Explosion.h
+++ Explosion.h
@@ -7,7 +7,7 @@ public:
     Explosion(int x, int y) : GameEntity(x, y, 'E') {}
 
     void apply(GameEntity& entity) override {
-        entity.setPos(-1, 1);
+        entity.setPos(-1, -1);
         entity.setType('X');
     }
 };
--- Ship.h
+++ Ship.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "GameEntity.h"
+using namespace std;
 
 class Ship : public GameEntity {
  public:
   Ship(int x, int y) : GameEntity(x, y, 'S') {}
 
   void move(int dx, int dy) {
-    int x = std::get<0>(getPos()) + dx;
-    int y = std::get<1>(getPos()) + dy;
+    int x = get<0>(getPos()) + dx;
+    int y = get<1>(getPos()) + dy;
     setPos(x, y);
   }
 };
